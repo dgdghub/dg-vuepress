44b4b34f4b5f842398247c0b2d2f7ae28b34f651
dgdghub <dgdghub@gmail.com>
2025-02-26 16:12:34 +0800

    init

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    title: 'AiEDU',
+    title: '',
     description: 'Just playing around',
     themeConfig: {
         logo: 'https://cdn.jsdelivr.net/gh/dgdghub/dg-pic@main/blog/202502250845177.png'
