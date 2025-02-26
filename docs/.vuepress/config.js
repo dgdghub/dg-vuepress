6c140f552ea84ab3f5649cca4ccaf47fe9e8e7bc
dgdghub <dgdghub@gmail.com>
2025-02-26 15:12:22 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    title: 'Hello VuePress',
+    description: 'Just playing around',
+    themeConfig: {
+        logo: 'https://cdn.jsdelivr.net/gh/dgdghub/dg-pic@main/blog/202502250845177.png'
+    }
+}
